c992b9b48badb2350d9c02ddaeee3a5230c589b2
Jared Soundy <jared_soundy@gmail.com>
2022-04-25 07:12:03 -0500

    Added max_time, updated print behavior.

--- solver/lib/parameters.py
+++ solver/lib/parameters.py
@@ -1,3 +1,4 @@
+import sys
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 
@@ -14,31 +15,38 @@ class Parameters:
     #   resolution.
     # cost_hi: int = 20000000
     # cost_lo: int = 0
-    cost_hi: int = 31092424832503900
-    cost_lo: int = 5624260931670580
-    cost_res: int = 1
+    cost_hi: int = 17227064385339908
+    # cost_lo: int =  5624260931670580
+    cost_lo: int =  5624260931670
+    cost_res: int = 10000
     # -Easy SAT and clearly UNSAT problems are solved quickly.
     # -The solver learns more from solving SAT problems.
     #   Therefore search begins at cost_hi and decreases each time by initial_hi_to_lo_ratio_step_up.
     #   Once the first UNSAT or UNKNOWN is encountered, after_failure_step_up_ratio is used instead.
     #   Set initial_hi_to_lo_ratio_step_up to zero to enter after_failure_step_up_ratio immediately.
-    initial_hi_to_lo_ratio_step_up: float = 1/1000
+    initial_hi_to_lo_ratio_step_up: float = 1/10000
     #   After first failure increase guess more conservatively.
-    after_failure_step_up_ratio: float = 1/10
+    after_failure_step_up_ratio: float = 1/1000
     # The number of miliseconds the solver should spend on any single iteration.
     #   Higher is better and slower.
     timeout: timedelta = timedelta(hours=2)
     # After a solver query is SAT, UNSAT, or UNKNOWN only print update to screen
     #   if at least update_time has passed since last printed update.
+    #   Prevents screen from being spammed with lots of SATs when problem is easy.
     #   First solver query always prints.
-    update_time: timedelta = timedelta(minutes=5)
+    update_time: timedelta = timedelta(seconds=0.01)
     # After a solver query is SAT only print update to file if at least sat_time
     #   has passed since last sat printed to file.
-    sat_time: timedelta = timedelta(minutes=10)
+    #   Prevents output file from being spammed with lots of SATs when problem is easy.
+    sat_time: timedelta = timedelta(seconds=0.01)
+    # Program will stop after max_time has been exceeded. This includes time of all runs,
+    #   but does not include setup time.
+    #   Will even quit mid-solve if max_time has been exceeded, but timeout has not.
+    max_time: timedelta = timedelta(minutes=5)
     # Contains the strings that will have chords assigned to them.
-    G_file: str = "G.txt"
+    G_file: str = "input/G_12.txt"
     # Frequency files to load:
-    H_file: str = "H.txt"
+    H_file: str = "input/H_12.txt"
     # Striding is assumed to be faster than normal, this is the discount given to strides.
     #   https://github.com/lancegatlin/typemax/blob/master/basic_layout_design.md#stride
     stride: float = 0.5
@@ -49,18 +57,20 @@ class Parameters:
     # Finger to reserve for switching between numbers, symbols, and alphabet.
     reserved_finger: str = "pinky"
 
+    def print_parameters(self, handle):
+        print("---------------------------------------------------------------------------------------------", file=handle)
+        print(f'Hi: {self.cost_hi}, Lo: {self.cost_lo}, Resolution: {self.cost_res}, Max Time: {self.max_time}', file=handle)
+        print(f'Timeout: {self.timeout}, Update Time: {self.update_time}, SAT Update Time: {self.sat_time}', file=handle)
+        print(f"Stride discount: {self.stride}, Stutter discount: {self.stutter}", file=handle)
+        print(f"Reserved finger: {self.reserved_finger}", file=handle)
+        print("---------------------------------------------------------------------------------------------", file=handle)
+
     def setup():
-        print("---------------------------------------------------------------------------------------------")
         p = Parameters()
         assert p.cost_hi > p.cost_lo
         assert p.cost_hi - p.cost_lo > p.cost_res
-        print(f'Hi: {p.cost_hi}, Lo: {p.cost_lo}, Resolution: {p.cost_res}')
-        print(f'Timeout: {p.timeout}, Update Time: {p.update_time}, SAT Update Time: {p.sat_time}')
-        print(f"Stride discount: {p.stride}, Stutter discount: {p.stutter}")
-        print(f"Reserved finger: {p.reserved_finger}")
-        # min_print_time replaced with update_time and sat_time. No need to print those out.
-        # print(f"If not last SAT solution print to STDOUT and file only if timer exceeds: {p.min_print_time}")
-        print("---------------------------------------------------------------------------------------------")
+        p.print_parameters(sys.stdout)
+        p.print_parameters(sys.stdout)
         return p
 
     def initial_step_up(self) -> float:
